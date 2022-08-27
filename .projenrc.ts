2d92c0c8e24c9eea5c48223b47c120bcecd10a49
Luka Hedt <lhedt@protonmail.com>
2022-08-27 14:17:12 +1000

    chore: project created with projen

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,9 +1,10 @@
-const { awscdk } = require('projen');
+import { awscdk } from 'projen';
 const project = new awscdk.AwsCdkTypeScriptApp({
   authorName: 'PhalanxHead',
   cdkVersion: '2.1.0',
   defaultReleaseBranch: 'main',
   name: 'AutoRotatingJwks',
+  projenrcTs: true,
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
